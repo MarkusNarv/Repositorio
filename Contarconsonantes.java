c0b6a3e8bb58189613bbc51b01d23823c50448c6
MarkusNarv <marcusdavidnarvaez@hotmail.com>
2023-03-20 13:11:20 -0300

    Primer version del programa

---
+++ Contarconsonantes.java
@@ -0,0 +1,25 @@
+
+public class Contarconsonantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto = "HOlA MUnDO como estas en este dia soleado";
+		int contadorConsonantes = 0;
+
+		for (int i = 0; i < texto.length(); i++) {
+		    char letra = texto.charAt(i);
+		    if (Character.isLetter(letra) && !isVowell(letra)) {
+		        contadorConsonantes++;
+		    }
+		}
+
+		System.out.println("El número de consonantes en el texto es: " + contadorConsonantes);
+	}
+
+	private static boolean isVowell(char letra) {
+		// TODO Auto-generated method stub
+		letra = Character.toLowerCase(letra);
+	    return letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u';
+	}
+
+}
